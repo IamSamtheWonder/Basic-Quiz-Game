c4b4d37b21836b258758fb356561e8923d551b03
Sam Awa <awasamuel85@yahoo.com>
2024-08-06 02:49:17 +0100

    Updated commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const questions = [
     {
-        questionText: "What is the capital of France?",
+        questionText: "Q.1: What is the capital of France?",
         options: ["Paris", "London", "Berlin", "Madrid"],
-        // correctAnswer: "Paris"
-        correctAnswer: 0
+        correctAnswer: "Paris"
+        // correctAnswer: 0
     },
     {
-        questionText: "What is the capital of United Kingdom?",
+        questionText: "Q.2: What is the capital of United Kingdom?",
         options: ["London", "Nairobi", "Paris", "Berlin"],
-        // correctAnswer: "London"
-        correctAnswer: 0
+        correctAnswer: "London"
+        // correctAnswer: 0
     },
     {
-        questionText: "Which of the following is not an animal?",
+        questionText: "Q.3: Which of the following is not an animal?",
         options: ["Goat", "Dog", "Samsung", "Elephant"],
-        // correctAnswer: "Samsung"
-        correctAnswer: 2
+        correctAnswer: "Samsung"
+        // correctAnswer: 2
     }
 ];
 
@@ -27,6 +27,7 @@ let score = 0;
 function displayQuestion() {
     const questionContainer = document.getElementById('question');
     const optionsContainer = Array.from(document.getElementsByClassName('option'));
+    console.log(optionsContainer);
 
     const currentQuestion = questions[currentQuestionIndex];
 
@@ -47,9 +48,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const options = document.querySelectorAll('.option');
     options.forEach(option => {
         option.addEventListener('click', () => {
-            const selectedOptionValue = option.value;
+            const selectedOptionValue = option.textContent;
             const currentQuestion = questions[currentQuestionIndex];
-
+            console.log(selectedOptionValue);
             // Check if the selected option matches the correct answer
 
             if (selectedOptionValue === currentQuestion.correctAnswer) {
@@ -61,14 +62,26 @@ document.addEventListener('DOMContentLoaded', () => {
             } else {
                 alert("Incorrect.");
             }
+            
+            // currentQuestionIndex++;
+            // if (currentQuestionIndex >= questions.length) {
+            //     alert(`Quiz completed! Your final score is ${score}/${questions.length}`);
+            //     document.getElementById('score').textContent = score;
+            //     // location.reload(); // Reload the page to restart the quiz
+            // } else if (currentQuestionIndex < questions.length) {
+            //     displayQuestion();
+            // } else {
+            //      location.reload(); // Reload the page to restart the quiz
+            // }
 
             currentQuestionIndex++;
             if (currentQuestionIndex >= questions.length) {
                 alert(`Quiz completed! Your final score is ${score}/${questions.length}`);
-                location.reload(); // Reload the page to restart the quiz
+                document.getElementById('score').textContent = score;
+                setTimeout(location.reload(), 5000); // Reload the page to restart the quiz
             } else {
                 displayQuestion();
-            }
+            } 
         });
     });
 });
